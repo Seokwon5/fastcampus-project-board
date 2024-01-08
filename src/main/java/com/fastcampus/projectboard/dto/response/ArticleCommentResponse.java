00fb8f838332ffccc4945449f051c3a8a2c78c35
Seokwon5 <tjrdnjs5@gmail.com>
2024-01-08 11:07:25 +0900

    #17 - 게시글 서비스 로직의 테스트와 골격 잡기
    게시글 검색하면 게시글 리스트 반환, 게시글을 id로 조회, 게시글 생성, 수정, 삭제

---
+++ src/main/java/com/fastcampus/projectboard/dto/response/ArticleCommentResponse.java
@@ -0,0 +1,52 @@
+package com.fastcampus.projectboard.dto.response;
+
+import com.fastcampus.projectboard.dto.ArticleCommentDto;
+import org.apache.logging.log4j.util.PropertySource;
+import org.hibernate.mapping.Set;
+
+import java.time.LocalDateTime;
+
+public record ArticleCommentResponse(
+        Long id,
+        String content,
+        LocalDateTime createdAt,
+        String email,
+        String nickname,
+        String userId,
+        Long parentCommentId,
+        Set<ArticleCommentResponse> childComments
+) {
+
+    public static ArticleCommentResponse of(Long id, String content, LocalDateTime createdAt, String email, String nickname, String userId) {
+        return ArticleCommentResponse.of(id, content, createdAt, email, nickname, userId, null);
+    }
+
+    public static ArticleCommentResponse of(Long id, String content, LocalDateTime createdAt, String email, String nickname, String userId, Long parentCommentId) {
+        PropertySource.Comparator<ArticleCommentResponse> childCommentComparator = Comparator
+                .comparing(ArticleCommentResponse::createdAt)
+                .thenComparingLong(ArticleCommentResponse::id);
+        return new ArticleCommentResponse(id, content, createdAt, email, nickname, userId, parentCommentId, new TreeSet<>(childCommentComparator));
+    }
+
+    public static ArticleCommentResponse from(ArticleCommentDto dto) {
+        String nickname = dto.userAccountDto().nickname();
+        if (nickname == null || nickname.isBlank()) {
+            nickname = dto.userAccountDto().userId();
+        }
+
+        return ArticleCommentResponse.of(
+                dto.id(),
+                dto.content(),
+                dto.createdAt(),
+                dto.userAccountDto().email(),
+                nickname,
+                dto.userAccountDto().userId(),
+                dto.parentCommentId()
+        );
+    }
+
+    public boolean hasParentComment() {
+        return parentCommentId != null;
+    }
+
+}
